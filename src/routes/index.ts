633b3a2e62ef4cad8dd5148b81ea0050fbf5dbb5
marcelogam <106875860+marcelogam@users.noreply.github.com>
2024-01-22 20:27:28 -0300

    base criada

---
+++ src/routes/index.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import * as SearchController from '../controllers/searchController';
+import * as PageController from '../controllers/pageController';
+
+const router = Router();
+
+router.get('/', PageController.home);
+router.get('/dogs', PageController.dogs);
+router.get('/cats', PageController.cats);
+router.get('/fishes', PageController.fishes);
+
+router.get('/search', SearchController.search);
+
+
+export default router;
